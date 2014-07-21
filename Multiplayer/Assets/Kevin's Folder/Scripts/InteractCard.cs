1ecdaa54493c8b4bedeb98f275a4bec3a5048c3d
Springo <kevinmxia@gmail.com>
2014-07-21 02:23:18 -0400

    Players draw 3 cards that can be played by clicking. Clicked cards go to the center and are replaced in the hand by another random card.
    Signed-off-by: Springo <kevinmxia@gmail.com>

---
+++ Multiplayer/Assets/Kevin's Folder/Scripts/InteractCard.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractCard : MonoBehaviour {
+	public GameObject omni;
+	public int cardplace;
+
+	// Use this for initialization
+	void Start () {
+		omni = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	void OnMouseDown () {
+		//sets the card clicked in the center as if it was played
+		transform.position = new Vector3 (0,0,0);
+		//changes the card at the card position of the clicked card to a new random card
+		omni.GetComponent<Player>().changeCard(cardplace);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//Method sets the cardplace variable to the given parameter "place"
+	void setCardPlace(int place) {
+		cardplace = place; 	
+	}
+}
+ 
---
+++ Multiplayer/Assets/Kevin's Folder/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public GameObject[] cardlist;
+	public GameObject[] cards;
+	public bool onTurn;
+
+	// Use this for initialization
+	void Start () {
+		onTurn = true;
+		//creates the player's hand at the beginning
+		for (int i = 0; i < cards.Length; i++) {
+			//creates a random card
+			GameObject clonedCard;
+			clonedCard = Instantiate (cardlist[Random.Range (0,3)]) as GameObject;
+			//sets each of the three card slots in the player's hand as a random card
+			cards[i] = clonedCard;
+			//places the cards in their corresponding positions on the scene
+			clonedCard.transform.position = new Vector3(-3 + i * 3, -3, 0);
+			//cards set their position in the array in their own script in InteractCard
+			clonedCard.SendMessage("setCardPlace", i);
+		}
+	}
+
+	//Method changes the card in slot "cardslot" of the player's hand to a random card
+	public void changeCard(int cardslot) {
+		//creates a random card
+		GameObject clonedCard;
+		clonedCard = Instantiate (cardlist[Random.Range (0,3)]) as GameObject;
+		//sets each of the three card slots in the player's hand as a random card
+		cards[cardslot] = clonedCard;
+		//places the cards in their corresponding positions on the scene
+		clonedCard.transform.position = new Vector3(-3 + cardslot * 3, -3, 0);
+		//cards set their position in the array in their own script in InteractCard
+		clonedCard.SendMessage("setCardPlace", cardslot);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
