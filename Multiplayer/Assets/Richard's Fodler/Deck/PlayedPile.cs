01dbacc86cf713a85e1c522eca5ed4c9f912b410
Richard Chen <richard.0.chen@gmail.com>
2014-07-21 02:27:26 -0400

    Signed-off-by: Richard Chen <richard.0.chen@gmail.com>

---
+++ Multiplayer/Assets/Richard's Fodler/Deck/PlayedPile.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayedPile : MonoBehaviour {
+	// number
+	private int currentNumber;
+	// Use this for initialization
+	void Start () {
+		currentNumber = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//add a card to pile obviously
+	public void addNumber(int i){
+		currentNumber += i;
+	}
+	
+	public int returnNumber(){
+		return currentNumber;
+	}
+	
+	//blowup the pile
+	public void resetPile(){
+		currentNumber = 0;
+	}
+}
---
+++ Multiplayer/Assets/Scripts/Server/NetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Name attributes for the server
+	private const string typeName = "UniqueGameName";
+	private const string gameName = "Brendan's Room";
+	private RoomInfo[] roomsList;
+
+	// Player prefab that will be spawned in
+	public GameObject playerPrefab;
+
+	// Calls on startup
+	void Start() {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	// GUI to host or join a server
+	void OnGUI() {
+		if (!PhotonNetwork.connected) {
+			GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null) {
+			// Create Room
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				PhotonNetwork.CreateRoom(gameName, true, true, 5);
+			
+			// Join Room
+			if (roomsList != null) {
+				for (int i = 0; i < roomsList.Length; i++) {
+					if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+						PhotonNetwork.JoinRoom(roomsList[i].name);
+				}
+			}
+		}
+	}
+
+	void OnReceivedRoomListUpdate() {
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+	void OnJoinedRoom() {
+		PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+	}
+}
