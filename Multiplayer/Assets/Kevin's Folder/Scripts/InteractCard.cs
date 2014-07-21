2412e341e582d4e957f4191bd24f7f5fffc7e7fd
Springo <kevinmxia@gmail.com>
2014-07-21 19:36:47 -0400

    Old played cards are now deleted after new cards are played.
    Signed-off-by: Springo <kevinmxia@gmail.com>

--- Multiplayer/Assets/Kevin's Folder/Scripts/InteractCard.cs
+++ Multiplayer/Assets/Kevin's Folder/Scripts/InteractCard.cs
@@ -2,20 +2,30 @@
 using System.Collections;
 
 public class InteractCard : MonoBehaviour {
-	public GameObject omni;
-	public int cardplace;
+	public GameObject omni, playedcard;
+	public int cardplace, value;
+	private bool played;
 
 	// Use this for initialization
 	void Start () {
 		omni = GameObject.FindGameObjectWithTag ("Player");
-
+		played = false;
 	}
 
 	void OnMouseDown () {
-		//sets the card clicked in the center as if it was played
-		transform.position = new Vector3 (0,0,0);
-		//changes the card at the card position of the clicked card to a new random card
-		omni.GetComponent<Player>().changeCard(cardplace);
+		if (played == false) {
+			//sets the card clicked in the center as if it was played
+			transform.position = new Vector3 (0, 0, 0);
+			//changes the card at the card position of the clicked card to a new random card
+			omni.GetComponent<Player> ().changeCard (cardplace);
+			//deletes the previous card played
+			playedcard = GameObject.FindGameObjectWithTag ("playedCard");
+			if (playedcard != null)
+				Destroy (playedcard);
+			//played cards have different properties from unplayed cards
+			played = true;
+			transform.gameObject.tag = "playedCard";
+		}
 	}
 
 	// Update is called once per frame
