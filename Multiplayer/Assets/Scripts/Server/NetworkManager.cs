5a0e29b8e3858d9aa69f5a8ce8093e91bbbc0918
Ludusamo <ludusamo@gmail.com>
2014-07-22 00:19:27 -0400

    Better GUI

--- Multiplayer/Assets/Scripts/Server/NetworkManager.cs
+++ Multiplayer/Assets/Scripts/Server/NetworkManager.cs
@@ -45,7 +45,7 @@ public class NetworkManager : MonoBehaviour {
 		GUI.Box (new Rect((Screen.width / 2) - (Screen.width / 8), (Screen.height / 20), Screen.width / 4, Screen.height / 10), "Available Server");
 		if (roomsList != null) {
 			for (int i = 0; i < roomsList.Length; i++) {
-				if (GUI.Button(new Rect((Screen.width / 2) - (Screen.width / 8), (Screen.height / 20) + ((Screen.height / 10) * (i + 1)), Screen.width / 4, Screen.height / 10), "Join " + roomsList[i].name))
+				if (GUI.Button(new Rect((Screen.width / 2) - (Screen.width / 8), (Screen.height / 20) + ((Screen.height / 10) * (i + 1)), Screen.width / 4, Screen.height / 10), "Join " + roomsList[i].name + " " + roomsList[i].playerCount + "/" + roomsList[i].maxPlayers))
 					PhotonNetwork.JoinRoom(roomsList[i].name);
 			}
 		}
