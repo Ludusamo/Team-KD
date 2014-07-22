19a80579b1c03a14774901ec6db62d3673f080c4
Springo <kevinmxia@gmail.com>
2014-07-22 00:02:45 -0400

    Scoreboard accessible by tab.
    Signed-off-by: Springo <kevinmxia@gmail.com>

---
+++ Multiplayer/Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour {
+	public int gold, lives;
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+		gold = lives = 0;
+		name = "TestName";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setName(string newName) {
+		name = newName;
+	}
+}
