dc28f42b164f8bd2e4cf82843c8cef5522ff7f4e
Harrding <harrisond2003@gmail.com>
2014-08-02 23:34:35 -0700

    Adding Network character to the master GUI Signed-off-by: Harrding <harrisond2003@gmail.com>

---
+++ Multiplayer/Assets/Scripts/GUI/NetworkMasterChar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMasterChar : MonoBehaviour {
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(((MasterGUI)gameObject.GetComponent<MasterGUI>()).stolengold);
+		}
+		else
+		{
+			// Network player, receive data
+			((MasterGUI)gameObject.GetComponent<MasterGUI>()).stolengold = (int)stream.ReceiveNext();
+
+		}
+	}
+}
